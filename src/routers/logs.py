1575787790a12ad5fa222fde16e3d964523e4aff
Prachi <prachi@kdexglobal.com>
2022-09-24 19:12:18 +0530

    combine aws and azure logs

--- src/routers/logs.py
+++ src/routers/logs.py
@@ -19,23 +19,21 @@ async def update_logs_insights(
     cloud_account_id: str,
     db: Session = Depends(auroradb.get_db)
 ):
-    # user_logs_aws_req_url = f'''{os.environ.get("USER_LOGS_URL")}accountID={account_id}&type=aws&cloudAccountID={cloud_account_id}'''
-    # response = requests.get(user_logs_aws_req_url)
-    # json_response = json.loads(response.text)
+    user_logs_aws_req_url = f'''{os.environ.get("USER_LOGS_URL")}accountID={account_id}&type=aws&cloudAccountID={cloud_account_id}'''
+    response = requests.get(user_logs_aws_req_url)
+    json_response = json.loads(response.text)
 
-    # logs = []
+    logs = []
 
-    # for logJSON in json_response:
-    #     log = InsightModel.from_aws_dict(logJSON,account_id,cloud_account_id)
-    #     logs.append(log)
+    for logJSON in json_response:
+        log = InsightModel.from_aws_dict(logJSON,account_id,cloud_account_id)
+        logs.append(log)
 
     insights_collection = LogInsightsCollection()  
 
-    # await insights_collection.upsert_insights(logs=logs,db=db)
+    await insights_collection.upsert_insights(logs=logs,db=db)
 
     user_logs_azure_req_url = f'''{os.environ.get("USER_LOGS_URL")}accountID={account_id}&type=azure&cloudAccountID={cloud_account_id}'''
-
-    f = open('C:/Users/Downloads/azure_logs.json','r',encoding="utf8")
     
     json_azure_data = json.load(f)
     azure_logs = []
